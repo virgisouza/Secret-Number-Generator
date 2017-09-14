612d83ba10fb1f622c7a3eb84ef9e086422ba76e
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-09-13 15:31:41 -1000

    secret num commit

--- secret_number.js
+++ secret_number.js
@@ -1,10 +1,8 @@
 'use strict';
 module.exports = function() {
+    var num = Math.floor(Math.random() * 1000000+1);
+  return function(){
+    return num;
+  }
 
-   function secretNumberGenerator(){
-      return ((Math.random() * 1000000)+1);
-    };
-
-
-        return secretNumberGenerator()
 };
