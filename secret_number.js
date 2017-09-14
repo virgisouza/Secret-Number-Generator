4aa31c1829a0afde2500c6a26cd06cc76e6a17ed
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-09-13 15:01:37 -1000

    secret num gen

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,10 @@
 'use strict';
 module.exports = function() {
-  
+
+   function secretNumberGenerator(){
+      return ((Math.random() * 1000000)+1);
+    };
+
+
+        return secretNumberGenerator()
 };
